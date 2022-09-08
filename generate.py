edafc6ab842f110f59b9d2abb2b025a8fc89f0c3
joshjohnstone <joshhjohnstone@gmail.com>
2022-09-08 07:56:04 +0200

    update requirements.txt

--- generate.py
+++ generate.py
@@ -74,15 +74,14 @@ def app():
                     message = f'''
                     Hi, can I please request *two* quotes:
 
-                    1. *USD/ZAR ask*
-                    2. *TUSD/ZAR bid*
+1. *USD/ZAR ask*
+2. *TUSD/ZAR bid*
 
-                    *Details*:
-                    
-                    - Full name: {name}
-                    - Trade amount: {zar}
-                    - FIA PIN: {fia}
-                    - Email: {email}'''
+*Details*:
+- Full name: {name}
+- Trade amount: {zar}
+- FIA PIN: {fia}
+- Email: {email}'''
 
                     st.markdown('### Your message to copy and paste')
 
